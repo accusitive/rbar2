ebc22be2a40d3707adfeff178ba58d6ce6f23a21
RS <rs@neb.best>
2021-10-03 13:07:18 -0400

    channels

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 use chrono::prelude::*;
-use parking_lot::Mutex;
-use std::{sync::Arc, time::Instant};
+use std::sync::{mpsc::Sender, Arc};
 #[macro_use]
 mod audio;
 mod bash;
 
 use crate::audio::{get_default_sink, get_headphones_volume, get_speakers_volume};
 trait Component<'a> {
-    fn update(&self);
-    fn get_content(&self) -> Arc<Mutex<String>>;
     fn get_delta(&self) -> u64;
     fn get_name(&self) -> String;
-}
+    fn get_tx(&'a self) -> &'a Arc<TXType>;
+    fn update() -> String;
+    fn get_from() -> From;
+    fn run(&'a self) {
+        let a = self.get_tx();
+        let t = a.as_ref().clone();
+        let duration = std::time::Duration::from_millis(self.get_delta());
 
+        std::thread::spawn(move || loop {
+            t.send((Self::get_from(), Self::update())).unwrap();
+            std::thread::sleep(duration);
+        });
+    }
+}
 struct Clock {
-    inner: Arc<Mutex<String>>,
+    inner: Arc<TXType>,
 }
 
 impl<'a> Component<'a> for Clock {
-    fn update(&self) {
+    fn update() -> String {
         let local: DateTime<Local> = Local::now();
-        *self.inner.lock() = local.format("📅 %a %b %e %I:%M:%S %P %Y").to_string();
-    }
-    fn get_content(&self) -> Arc<Mutex<String>> {
-        self.inner.clone()
+        local.format("📅 %a %b %e %I:%M:%S %P %Y").to_string()
     }
 
     fn get_delta(&self) -> u64 {
@@ -33,12 +39,21 @@ impl<'a> Component<'a> for Clock {
     fn get_name(&self) -> String {
         "Clock".to_string()
     }
+
+    fn get_tx(&'a self) -> &'a Arc<TXType> {
+        &self.inner
+    }
+
+    fn get_from() -> From {
+        From::Clock
+    }
 }
+
 struct VolumeLevel {
-    inner: Arc<Mutex<String>>,
+    inner: Arc<TXType>,
 }
 impl<'a> Component<'a> for VolumeLevel {
-    fn update(&self) {
+    fn update() -> String {
         let default_sink = get_default_sink();
         let (h, s) = match default_sink.trim() {
             speakers!() => (" 🎧 ", "[ 🔊 ]"),
@@ -52,13 +67,9 @@ impl<'a> Component<'a> for VolumeLevel {
             s,
             get_speakers_volume()
         );
-        {
-            *self.inner.lock() = f;
-        }
-    }
 
-    fn get_content(&self) -> Arc<Mutex<String>> {
-        self.inner.clone()
+        f
+        // *self.inner.lock() = f;
     }
 
     fn get_delta(&self) -> u64 {
@@ -68,23 +79,24 @@ impl<'a> Component<'a> for VolumeLevel {
     fn get_name(&self) -> String {
         "VolumeLevel".to_string()
     }
+
+    fn get_tx(&'a self) -> &'a Arc<TXType> {
+        &self.inner
+    }
+    fn get_from() -> From {
+        From::Audio
+    }
 }
 struct Weather {
-    inner: Arc<Mutex<String>>,
+    inner: Arc<TXType>,
 }
 impl<'a> Component<'a> for Weather {
-    fn update(&self) {
-        let weather =
-            bash::exec(r#"curl -s "wttr.in/?format=1" | grep -o ".[0-9].*""#.to_string());
+    fn update() -> String {
+        // bash::exec(r#"curl -s "wttr.in/?format=1" | grep -o ".[0-9].*""#.to_string());
+        let weather = bash::exec(r#"curl -s "wttr.in/?format=1""#.to_string());
         let weather = weather.trim();
-        println!("{:#?}", weather);
-        {
-            *self.inner.lock() = format!("🌡{}", weather)
-        }
-    }
 
-    fn get_content(&self) -> Arc<Mutex<String>> {
-        self.inner.clone()
+        format!("{}", weather)
     }
 
     fn get_delta(&self) -> u64 {
@@ -94,53 +106,58 @@ impl<'a> Component<'a> for Weather {
     fn get_name(&self) -> String {
         "Weather".to_string()
     }
+
+    fn get_tx(&'a self) -> &'a Arc<TXType> {
+        &self.inner
+    }
+
+    fn get_from() -> From {
+        From::Weather
+    }
 }
+
+#[derive(Debug)]
+enum From {
+    Audio,
+    Clock,
+    Weather,
+}
+type TXType = Sender<(From, String)>;
+
 fn main() {
     let display = unsafe { x11::xlib::XOpenDisplay(0 as *const i8) };
     let root = unsafe { x11::xlib::XRootWindow(display, 0) };
-    let mut title = String::with_capacity(256);
-    let clock = Clock {
-        inner: Arc::new(Mutex::new(String::new())),
-    };
+    let mut title;
+    let (tx, rx) = std::sync::mpsc::channel::<(From, String)>();
 
-    let volume_level = VolumeLevel {
-        inner: Arc::new(Mutex::new(String::new())),
-    };
-    let weather = Weather {
-        inner: Arc::new(Mutex::new(String::new())),
-    };
-    let components: Vec<Arc<dyn Component + Send + Sync>> =
-        vec![Arc::new(clock), Arc::new(volume_level), Arc::new(weather)];
-
-    for component in components.clone() {
-        let ac = Arc::new(component);
-
-        let _t = std::thread::spawn(move || loop {
-            {
-                let now = Instant::now();
-                ac.update();
-                let e = now.elapsed().as_millis();
-                println!("{} took {}ms", ac.get_name(), e);
-            }
-            std::thread::sleep(std::time::Duration::from_millis(ac.get_delta()));
-        });
+    Clock {
+        inner: Arc::new(tx.clone()),
     }
-    loop {
-        // time
+    .run();
+    VolumeLevel {
+        inner: Arc::new(tx.clone()),
+    }
+    .run();
+    Weather {
+        inner: Arc::new(tx.clone()),
+    }
+    .run();
 
-        title.clear();
-        for component in components.clone() {
-            title.push_str(&component.get_content().lock());
-            title.push_str(" | ");
+    let mut clock = String::new();
+    let mut audio = String::new();
+    let mut weather = String::new();
+
+    for r in rx.iter() {
+        match r.0 {
+            From::Audio => audio = r.1,
+            From::Clock => clock = r.1,
+            From::Weather => weather = r.1,
         }
-        // title.push_str(&local.format("%a %b %e %I:%M:%S %P %Y").to_string());
+        title = format!("{} {} {}", audio, clock, weather);
         title.push('\0');
-        // cpu temp
-
         unsafe { x11::xlib::XStoreName(display, root, title.as_ptr() as *const i8) };
         unsafe {
             x11::xlib::XFlush(display);
         }
-        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
